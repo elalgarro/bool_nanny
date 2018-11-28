d1cd5af6274936dec8a263250bcde48d5c1d53b2
Andrew Algard <andrewalgard@gmail.com>
2018-11-27 17:16:10 -0800

    default to true and false

--- lib/generators/change_null_bool_defaults_to_false.rb
+++ lib/generators/change_null_bool_defaults_to_false.rb
@@ -1,4 +1,4 @@
-require_relative(Rails.root + '/config/environment.rb')
+require_relative(Rails.root.to_s + '/config/environment.rb')
 
 puts 'creating migration...'
 system "rails g migration AddDefaultValueToBooleans"
@@ -17,30 +17,32 @@ def replace(filepath, regexp, *args, &block)
 end
 
 def get_bool_cols
-  @bool_cols ||= {}
-  if @bool_cols.empty?
+  @bool_cols ||= {'true'=>{}, 'false'=>{}}
+  if @bool_cols['true'].empty? && @bool_cols['false'].empty? 
     Dir[Rails.root.join('app/models/*.rb').to_s].each do |filename|
       klass = File.basename(filename, '.rb').camelize.constantize
       next unless klass.ancestors.include?(ActiveRecord::Base)
       next if klass.abstract_class?
       klass.columns.each do |c| 
         if c.type.to_s == 'boolean' && c.null.to_s == 'true'
-          update_nulls_to_false klass, c.name.to_sym
-          if @bool_cols[klass.table_name.to_sym].present?
-           @bool_cols[klass.table_name.to_sym] << c.name.to_sym
-         else
-           @bool_cols[klass.table_name.to_sym] = [c.name.to_sym]
-         end
+          t_or_f = c.default ? 'true' : 'false'
+          update_nulls_to_false klass, c.name.to_sym, t_or_f
+          if @bool_cols[t_or_f][klass.table_name.to_sym].present?
+            @bool_cols[t_or_f][klass.table_name.to_sym] << c.name.to_sym
+          else
+            @bool_cols[t_or_f][klass.table_name.to_sym] = [c.name.to_sym]
+          end
         end
       end
     end
   end
   @bool_cols
 end
-def update_nulls_to_false model, attr
+def update_nulls_to_false model, attr, value
   puts "updating #{model}, #{attr}..."
+  value = value == "true"
   working = true
-  unless model.unscoped.where(attr => nil).update_all(attr.to_sym => false)
+  unless model.unscoped.where(attr => nil).update_all(attr.to_sym => value)
       working = false
   end
   if working 
@@ -51,14 +53,20 @@ def update_nulls_to_false model, attr
 end
 get_bool_cols
 puts 'populating  migration field'
+
 replace( file, /..def.change/mi) { |match| "
   def change
-    #{get_bool_cols}.each do |table, attrs|
+    #{get_bool_cols['false']}.each do |table, attrs|
       attrs.each do |attr|
-        default = table.to_s == 'projects_users' && attr.to_s == 'onsite'
-        change_column table, attr, :boolean, null: false, default: default
+        change_column table, attr, :boolean, null: false, default: false
       end
-    end" }  
+    end
+    #{get_bool_cols['true']}.each do |table, attrs|
+      attrs.each do |attr|
+        change_column table, attr, :boolean, null: false, default: true
+      end
+    end
+    " }  
 
 puts "successfully populated migration"
 puts "migrating..."
